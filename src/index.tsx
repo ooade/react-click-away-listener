a5f3cdd7a08eec59e89172c8f3b24e920d77af48
Ademola Adegbuyi <ooade96@gmail.com>
2025-04-12 23:58:26 +0100

    fix: react19 support

--- src/index.tsx
+++ src/index.tsx
@@ -33,22 +33,21 @@ const eventTypeMapping = {
 	touchend: 'onTouchEnd'
 };
 
-const reactMajorVersion = parseInt(React.version.split('.')[0], 10);
-console.log({ reactMajorVersion });
-
-function useForkRef<T = any>(
-	...refs: Array<Ref<T> | undefined>
-): RefCallback<T> {
-	return (node: T) => {
+const mergeRefs = <T extends any>(
+	refs: Array<Ref<T> | undefined | null>
+): RefCallback<T> => {
+	return (value) => {
 		refs.forEach((ref) => {
 			if (typeof ref === 'function') {
-				ref(node);
-			} else if (ref != null && typeof ref === 'object') {
-				(ref as MutableRefObject<T | null>).current = node;
+				ref(value);
+			} else if (ref != null) {
+				(ref as MutableRefObject<T | null>).current = value;
 			}
 		});
 	};
-}
+};
+
+const reactMajorVersion = parseInt(React.version.split('.')[0], 10);
 
 const ClickAwayListener: FunctionComponent<Props> = ({
 	children,
@@ -87,13 +86,17 @@ const ClickAwayListener: FunctionComponent<Props> = ({
 			}
 		};
 
-	const combinedRef = useForkRef((ref) => {
-		node.current = ref;
-	}, (children as any).ref);
+	let childRef: React.Ref<any> | null = null;
 
-	const handleReact19ChildRef = (instance: HTMLElement | null) => {
-		node.current = instance;
-	};
+	// For React 19+, we get the ref via props.ref
+	if (reactMajorVersion >= 19) {
+		childRef = children.props?.ref || null;
+	} else if ('ref' in children) {
+		childRef = (children as any).ref;
+	}
+
+	// Create a combined ref handler
+	const combinedRef = mergeRefs([node, childRef]);
 
 	useEffect(() => {
 		const nodeDocument = node.current?.ownerDocument ?? document;
@@ -129,7 +132,7 @@ const ClickAwayListener: FunctionComponent<Props> = ({
 
 	return React.Children.only(
 		cloneElement(children as ReactElement<any>, {
-			ref: reactMajorVersion >= 19 ? handleReact19ChildRef : combinedRef,
+			ref: combinedRef,
 			[mappedFocusEvent]: handleBubbledEvents(mappedFocusEvent),
 			[mappedMouseEvent]: handleBubbledEvents(mappedMouseEvent),
 			[mappedTouchEvent]: handleBubbledEvents(mappedTouchEvent)
